e726260944a3a11817900120da22670e17ecc277
Frank / <144677258+frankosparks@users.noreply.github.com>
2024-02-19 22:58:01 +0300

    initial
    initial commit

---
+++ OOP 10-stars/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP_10_stars;
+class Program
+{
+    static void Main()
+    {
+       Console.WriteLine("Enter a number:");
+       int n=Convert.ToInt32(Console.ReadLine()); 
+       for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=i;j++){
+            Console.Write("*");
+        }
+        Console.WriteLine();
+       }
+    }
+}
